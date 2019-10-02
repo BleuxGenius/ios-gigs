a2217c5177b3d25ebf39fbafe3593329982eae2a
Dani Blackwell <bleuxgenius@gmail.com>
2019-10-02 15:59:46 -0500

    setting Up the UI

---
+++ GigApp/GigApp/View Controller/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  GigApp
+//
+//  Created by Lambda_School_Loaner_167 on 10/2/19.
+//  Copyright © 2019 Dani. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
